621f2786057f35e34451bc184605af46e037b2e9
WhitesLove <kubai.rita2@yandex.ru>
2021-05-21 17:38:54 +0300

    Move token to config

--- bot.js
+++ bot.js
@@ -1,3 +1,3 @@
+const token = require('/config');
 const TelegramBot = require('node-telegram-bot-api');
-const token = '1750945348:AAEYGISgU-vV3NZOJ0KbhDAm9FneEhlzCPo';
 module.exports = new TelegramBot(token, {polling: true});
